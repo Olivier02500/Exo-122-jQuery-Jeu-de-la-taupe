98ea3c3458f3e058dc2f2ad471014b7e5b15f584
Olivier02500 <olivier.peuchet@hotmail.com>
2021-12-22 10:01:23 +0100

    .

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $('.hole');
+const scoreBoard = $('.score');
+const moles = $('.mole');
+
+startGame = function (){
+
+}
+
+$('#startGame').click(event => {
+
+    startGame()
+})
+
